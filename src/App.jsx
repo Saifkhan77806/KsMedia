250abb5f8dad632003751703956bd2e66d2d17e7
saifkhan77806 <saifkhan77806@gmail.com>
2024-08-13 09:11:00 +0530

    navbar is added with profile icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import logo from "../images/logo2.png"
 import './App.css'
 import Register from '../coomponents/Register';
+import Login from "../coomponents/Login"
 import Home from '../coomponents/Home';
 import UserBlog from '../coomponents/UserBlog';
 import CreateBlog from '../coomponents/CreateBlog';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import Blog from '../coomponents/Blog';
 import EditComment from '../coomponents/EditComment';
 import DeleteComment from '../coomponents/DeleteComment';
+import Profile from "../coomponents/Profile"
+import { StickyNavbar } from '../coomponents/Navbar';
 
 
 function App() {
@@ -15,12 +19,13 @@ function App() {
 
 
   return (
-    <>  <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <> 
+   <StickyNavbar />
     <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Route path='/register' element={<Register />}/>
+        <Route path='/login' element={<Login />}/>
         <Route path="/blog/:id/edit-comment/:blogid/:commentid" element={<EditComment />}/>
         <Route path="/blog/:id/delete-comment/:blogid/:commentid" element={<DeleteComment />}/>
     </Routes>
